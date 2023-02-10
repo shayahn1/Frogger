1b5455439c6feebed4f5b771f59f8120ddf87201
Pek, Aidan Lyons <apek3@gatech.edu>
2023-02-09 21:09:57 -0500

    Implement second activity for player config

--- app/src/main/java/com/example/frogger2340project/MainActivity.java
+++ app/src/main/java/com/example/frogger2340project/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.frogger2340project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button startGameButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startGameButton = (Button) findViewById(R.id.startButton);
+
+        startGameButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, PlayerConfig.class);
+            startActivity(intent);
+        });
     }
 }
