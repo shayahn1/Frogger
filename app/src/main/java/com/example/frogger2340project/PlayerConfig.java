0ebc3debb42e0d5f99c21ac4a42faf0cf84d1530
Pek, Aidan Lyons <apek3@gatech.edu>
2023-02-10 11:19:35 -0500

    Adding playername edit text box

--- app/src/main/java/com/example/frogger2340project/PlayerConfig.java
+++ app/src/main/java/com/example/frogger2340project/PlayerConfig.java
@@ -3,12 +3,19 @@ package com.example.frogger2340project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class PlayerConfig extends AppCompatActivity {
 
+    EditText playerNameText;
+    public String playerName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_config);
+
+
+        playerNameText = findViewById(R.id.playerName);
+        playerName = playerNameText.getText().toString();
     }
 }
