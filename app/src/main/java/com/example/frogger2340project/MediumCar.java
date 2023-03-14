b0a557f96937e772d1afc7cfab13a38d0b864509
Pek, Aidan Lyons <apek3@gatech.edu>
2023-03-14 15:57:15 -0400

    Fix car sizing, two more rows, and 3 more tests

--- app/src/main/java/com/example/frogger2340project/MediumCar.java
+++ app/src/main/java/com/example/frogger2340project/MediumCar.java
@@ -12,10 +12,18 @@ public class MediumCar {
     int vehicleX, vehicleY, vehicleVelocity;
     Random random;
 
+    public MediumCar(int x, int y, int veloc) {
+        vehicleX = x;
+        vehicleY = y;
+        vehicleVelocity = veloc;
+    }
     public MediumCar(Context context) {
         medium[0] = BitmapFactory.decodeResource(context.getResources(), R.drawable.mediumcar);
+        medium[0] = Bitmap.createScaledBitmap(medium[0], medium[0].getWidth() * 2, medium[0].getHeight() * 2, false);
         medium[1] = BitmapFactory.decodeResource(context.getResources(), R.drawable.mediumcar);
+        medium[1] = Bitmap.createScaledBitmap(medium[1], medium[1].getWidth() * 2, medium[1].getHeight() * 2, false);
         medium[2] = BitmapFactory.decodeResource(context.getResources(), R.drawable.mediumcar);
+        medium[2] = Bitmap.createScaledBitmap(medium[2], medium[2].getWidth() * 2, medium[2].getHeight() * 2, false);
         random = new Random();
         resetPosition();
     }
@@ -36,4 +44,21 @@ public class MediumCar {
         vehicleY = 1534;
         vehicleVelocity = 5; //+ random.nextInt(16);
     }
+
+    public void resetPosition2(int width) {
+        vehicleX = GameView.getDeviceWidth() + width;
+        vehicleY = 1338;
+        vehicleVelocity += 5; //+ random.nextInt(16);
+    }
+
+    public void resetPosition3(int width) {
+        vehicleX = 0 - width;
+        vehicleY = 1240;
+        vehicleVelocity = 1;
+    }
+
+    public void accelerate() {
+        vehicleVelocity++;
+    }
+
 }
--- app/src/test/java/com/example/frogger2340project/Sprint3Test.java
+++ app/src/test/java/com/example/frogger2340project/Sprint3Test.java
@@ -2,17 +2,21 @@ package com.example.frogger2340project;
 
 import static org.junit.Assert.assertEquals;
 
+import android.content.Context;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class Sprint3Test {
     private Frog frog1;
     private Frog frog2;
+    private MediumCar mediumCar;
 
     @Before
     public void init() {
         frog1 = new Frog();
         frog2 = new Frog(500, 1730, 10);
+        mediumCar = new MediumCar(50, 50, 5);
     }
 
     //Shayahn Mirfendereski
@@ -47,5 +51,30 @@ public class Sprint3Test {
         frog1.moveFrogDown(1730);
         frog1.moveFrogUp(1632);
         assertEquals(5, frog1.getScore(), 0);
+    }
+
+    @Test
+    public void testCreateMediumCar() {
+        mediumCar = new MediumCar(50, 60, 100);
+        assertEquals(50, mediumCar.vehicleX, 0);
+        assertEquals(60, mediumCar.vehicleY, 0);
+        assertEquals(100, mediumCar.vehicleVelocity, 0);
+    }
+    @Test
+    public void testVehicleVelocity() {
+        assertEquals(5, mediumCar.vehicleVelocity, 0);
+        mediumCar.resetPosition2(50);
+        assertEquals(10, mediumCar.vehicleVelocity, 0);
+        mediumCar.resetPosition2(50);
+        assertEquals(15, mediumCar.vehicleVelocity, 0);
+    }
+
+    @Test
+    public void testAcceleration() {
+        assertEquals(5, mediumCar.vehicleVelocity, 0);
+        for(int i = 0; i < 5; i++) {
+            mediumCar.accelerate();
+        }
+        assertEquals(10, mediumCar.vehicleVelocity, 0);
     }
 }
